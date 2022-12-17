b6fd5ec582946a7ad7312c3bb3e8e1393c0bec42
Marcos Castro <marcosmax.5@gmail.com>
2022-12-17 20:14:30 -0300

    Editando tipo de chat

--- src/pages/UI/interface.jsx
+++ src/pages/UI/interface.jsx
@@ -2,19 +2,19 @@ function Interface() {
     return (
       <body>
         <div class="container">
-         
+        <br />
           <div class="jumbotron">
             <h1 class="display-4">Send Message</h1>
-            
+            <br />
             <input id = "name" class="form-control" placeholder="Name"/>
-            
+            <br />
             <textarea id = "message" class="form-control" placeholder="Your Message Here">
             </textarea>
-            
+            <br />
             <button id="send" class="btn btn-success">Send</button>
           </div>  
             <div id="messages">
-            
+            <br />
             </div>
         </div>
         <script>
