0c8b3539e11b62cee7909144c8e9c422395ebd05
Marcos Castro <marcosmax.5@gmail.com>
2022-12-22 21:24:39 -0300

    Adicionando o componente que envia mensagens

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,13 +1,44 @@
+import { useState } from "react";
 import { sendMessage, isTyping } from "react-chat-engine";
 
 const MessageForm = (props) => {
+    const [text, setText] = useState('');
+    const [isTyping, setTyping] = useState(false);
+    const {creds, chatId} = props;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        if (text.length > 0){
+            sendMessage(creds, chatId, { text });
+            setText('');
+        }
+    }
+
+    const handleChange = (event) => {
+        const text_msg = event.target.value;     
+        setText(text_msg);
+
+        if (text.length > 0){
+            setTyping(true);
+        }else{
+            setTyping(false)
+        }
+    }
+
     return(
-        <form className="message-form">
-            <input
-               className="message-input"
-               placeholder="Envie sua mensagem"
-               />
-        </form>
+        <div>     
+            {isTyping ? <div>está digitando... </div> : <div/>}   
+                <form className="message-form" onSubmit={handleSubmit}>
+                    <input
+                    className="message-input"
+                    placeholder="Envie sua mensagem"
+                    vslue={text}
+                    onChange={handleChange}
+                    onSubmit={handleSubmit}
+                    />
+                </form>
+        </div>
     )
 
 }
